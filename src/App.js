3d68d55944228f1e57d6738f4f6886cdb69489d0
rolandexplore93 <roland2rule@gmail.com>
2022-04-21 07:46:10 +0100

    Add bg-image and create the components layouts and styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Details from './components/Details/Details'
+import {Grid} from '@material-ui/core'
+
 function App() {
   return (
     <div className="App">
       Budget Tracker App
+      <Grid container spacing={0} alignItems='center' justifyContent='center' style={{height: '100vh'}}>
+        <Grid item xs={12} sm={4}>
+            <Details />
+        </Grid>
+        <Grid item xs={12} sm={4}>
+            Main
+        </Grid>
+        <Grid item xs={12} sm={4}>
+            <Details />
+        </Grid>
+
+      </Grid>
+      
     </div>
   );
 }
